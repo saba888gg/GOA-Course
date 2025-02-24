175187e7736aeddeb4d4175c64122e040fc43696
saba888gg <sabasomkhishvili16@gmail.com>
2025-02-24 18:55:28 +0400

    third commit

---
+++ day 2/homeworck/homeworck1.py
@@ -0,0 +1,5 @@
+
+num1 = 400  #integer
+
+misamarti = "dushetis raioni sofeli wilurti"  #string
+
---
+++ day 2/homeworck/homeworck2.py
@@ -0,0 +1,19 @@
+
+misalmeba = "gamarjoba"
+
+elementi = "elementi " *10
+
+num = 500 + 500
+
+haloo = "haloo " + "world"
+
+gatoleba1 = 30 < 10
+
+gatoleba2 = 50 == 50
+
+print(misalmeba)
+print(elementi)
+print(num)
+print(haloo)
+print(gatoleba1)
+print(gatoleba2)
